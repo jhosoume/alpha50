389a01a8bbc8abc54ab971a2c68b21a41c8c98bd
triple-eh <aleckaleksandrov@gmail.com>
2016-07-11 20:01:22 -0700

    Added search functionality for the stock area
    -The list of stock quotes will filter live based on search

--- public/js/stock_side_bar.js
+++ public/js/stock_side_bar.js
@@ -1,10 +1,35 @@
 $(function() {
-  var stocks = JSON.parse(JSON.stringify($(".stock-bar-data").data('stock-data')));
+  var allStocks = JSON.parse(JSON.stringify($(".stock-bar-data").data('stock-data')));
   var hbsSource = $('#stock-quote-hbs').html();
   var template = Handlebars.compile(hbsSource);
+  var timer;
 
 
-  renderStockBar(stocks);
+  renderStockBar(allStocks);
+
+
+  $('.stock-side-bar').on('keyup', 'input', function() {
+    clearQuoteArea();
+    var searchTerm = $(this).val();
+    // ensure that the search term is alphanumeric before creating
+    // regex based on the search
+    var alphaNum = new RegExp(/^\w+$/);
+    if (!alphaNum.test(searchTerm)) return false;
+
+    var re = new RegExp(searchTerm, "i");
+    var filtered = allStocks.filter(function(stock) {
+      return re.test(stock.tic);
+    });
+    window.clearTimeout(timer);
+    timer = setTimeout(function() {
+      renderStockBar(filtered);
+    },400)
+
+  })
+
+  function clearQuoteArea() {
+    $(".stock-quote-area").empty();
+  }
 
   function renderStockBar(stocks) {
     $.each(stocks, function(idx, stock) {
