1fb12a9b1130e5ba78d917415cc6dfb333b2e970
Matt Breden <matt_bred@hotmail.com>
2016-07-12 17:48:04 -0700

    Add first API route in routes.php

--- config/routes.php
+++ config/routes.php
@@ -3,3 +3,8 @@ namespace Spark\Config;
 
 get('/', ['function'=>'index']);
 get('/portfolios', ['function'=>'index']);
+
+get('/api/stocks/:symbol', [
+	'function'=>'index',
+	'namespace'=>'api'
+]);
