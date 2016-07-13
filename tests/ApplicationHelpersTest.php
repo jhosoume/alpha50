8bd04a2d30a7c86da5f6865eb45fc53035571a22
Matt Breden <matt_bred@hotmail.com>
2016-07-13 15:31:29 -0700

    Add phpunit and first test

---
+++ tests/ApplicationHelpersTest.php
@@ -0,0 +1,14 @@
+<?php
+use PHPUnit\Framework\TestCase;
+
+class ApplicationHelpersTest extends TestCase {
+	public function testCurrentUser()
+    {
+    	$this->assertEquals(false, current_user());
+
+    	$user = User::create(['email'=>'asdf@gmail.com', 'password_hash'=>'asdf']);
+    	$_SESSION['user_id'] = User::first()->id;
+    	$this->assertEquals(User::first(), current_user());
+    	$user->delete();
+    }
+}
