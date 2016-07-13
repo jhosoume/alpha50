c7dd9b3f3334047d22debd7b084f933f0be692da
Matt Breden <matt_bred@hotmail.com>
2016-07-13 14:44:20 -0700

    Add callback for trade after_create that adjusts stocks_portfolio quantity_held

--- app/models/trade.php
+++ app/models/trade.php
@@ -4,6 +4,19 @@ class Trade extends ActiveRecord\Model implements JsonSerializable {
 		array('stocks_portfolio')
 	);
 
+	static $after_create = array('adjust_stocks_portfolios_quantity_held');
+
+	public function adjust_stocks_portfolios_quantity_held() {
+		/* 
+			This adjusts the stocks_portfolio's quantity held.
+			For ex, Johns portfolio has no aapl stock. It then buys 10 aapl.
+			This callback fires, and John/aapl stocks_portfolio quantity_held is now 10.
+		*/
+		$stocks_portfolio = $this->stocks_portfolio;
+		$stocks_portfolio->quantity_held += $this->quantity;
+		$stocks_portfolio->save();
+	}
+
 	public function jsonSerialize()
     {
         return json_decode($this->to_json());
