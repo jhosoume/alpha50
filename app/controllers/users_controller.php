d103b16d88d2c233a683c43bfb1c8a2172590e5e
Matt Breden <matt_bred@hotmail.com>
2016-07-13 20:53:10 -0700

    Add user validations
    move all password hasing from users_controller to user model
    add validations on create for user to check password length
    add tests for user creation, checking password and email

--- app/controllers/users_controller.php
+++ app/controllers/users_controller.php
@@ -2,11 +2,10 @@
 class UsersController extends Spark\BaseController {
   public function create() {
     $user_email = $this->params['user_email'];
-    $password_hash = password_hash($this->params['user_password'], PASSWORD_DEFAULT);
 
     $user = new User();
     $user->email = $user_email;
-    $user->password_hash = $password_hash;
+    $user->password = $this->params['user_password'];
     
     if ($user->save()) {
       $this->locals = array('message' => 'All good');
--- app/models/user.php
+++ app/models/user.php
@@ -1,13 +1,37 @@
 <?php
 class User extends ActiveRecord\Model implements JsonSerializable {
-	static $has_many = array(
-		array('portfolios')
-	);
+  // Plain text - only used on user creation.
+  private $plain_text_password;
 
+  // Function for active record to get the plain text password on validations.
+  protected function get_plain_text_password() {
+    return $this->plain_text_password;
+  }
+
+  static $has_many = array(
+  	array('portfolios')
+  );
 
   static $validates_presence_of = array(
     ['email'],
+    ['plain_text_password', 'on'=>'create']
   );
+
+  static $validates_size_of = array(
+    ['plain_text_password', 'on'=>'create', 'minimum'=>8, 'too_short'=>'must be at least 8 characters long']
+  );
+
+  static $after_create = array('nullify_password');
+
+  // ActiveRecord attribute setter. User passwords are always set with user->password = "password".
+  public function set_password($plain_text) {
+    $this->plain_text_password = $plain_text; // Only for save validation
+    $this->password_hash = password_hash($plain_text, PASSWORD_DEFAULT);
+  }
+
+  public function nullify_password() {
+    $this->plain_text_password = null;
+  }
 
   public function authenticate($password) {
     return password_verify($password, $this->password_hash);
