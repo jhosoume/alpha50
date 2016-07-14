881183e76405ff9f89c81456881a0b08fd257f58
triple-eh <aleckaleksandrov@gmail.com>
2016-07-13 19:52:26 -0700

    Added interportfolio dropdown

--- app/views/portfolios/portfolio-area.php
+++ app/views/portfolios/portfolio-area.php
@@ -1,14 +1,34 @@
-<div class="portfolio-area col s12 m10 l10">
-  <div class="container">
-    <?php render_file('/portfolios/portfolio-tab/portfolio_nav.php') ?>
-    <div class='portfolio-content'>
-      <?php render_file('/portfolios/portfolio-tab/overview.php') ?>
-      <?php render_file('/portfolios/portfolio-tab/all_stocks.php') ?>
-      <?php render_file('/portfolios/portfolio-tab/analysis.php') ?>
-      <?php render_file ('portfolios/portfolio-tab/comparison.php') ?>
-      <?php render_file('/portfolios/portfolio-tab/trades.php') ?>   
+<div class="portfolio-section col s12 m10 l10">
+
+    <div class="container">
+      <div class="row">
+<!--         <nav>
+          <ul class="right hide-on-med-and-down">
+            <li><a href="#!">First Sidebar Link</a></li>
+            <li><a href="#!">Second Sidebar Link</a></li>
+          </ul>
+          <ul id="slide-out" class="side-nav">
+            <li><a href="#!">First Sidebar Link</a></li>
+            <li><a href="#!">Second Sidebar Link</a></li>
+          </ul>
+          <a href="#" data-activates="slide-out" class="button-collapse"><i class="mdi-navigation-menu">reo</i></a>
+        </nav> -->
+
+
+        <div class='col s11 intra-portfolio-area'>
+          <?php render_file('/portfolios/portfolio-tab/portfolio_nav.php') ?>
+          <div class='portfolio-content'>
+           <?php render_file('/portfolios/portfolio-tab/overview.php') ?>
+           <?php render_file('/portfolios/portfolio-tab/all_stocks.php') ?>
+           <?php render_file('/portfolios/portfolio-tab/analysis.php') ?>
+            <?php render_file ('portfolios/portfolio-tab/comparison.php') ?>
+            <?php render_file('/portfolios/portfolio-tab/trades.php') ?>   
+          </div>
+        </div>
+
+
+      </div>
     </div>
-  </div>
 </div>
 
 <?php render_file('/layouts/preloader.php') ?>
--- app/views/portfolios/portfolio-tab/portfolio_nav.php
+++ app/views/portfolios/portfolio-tab/portfolio_nav.php
@@ -1,13 +1,17 @@
 <nav class="portfolio-nav grey darken-2">
-  <div class="container">
-    <div>
-      <ul>
-        <li><a href="#overview-tab">Overview</a></li>
-        <li><a href="#all-stocks-tab">All Stocks</a></li>
-        <li><a href="#analysis-tab">Analysis</a></li>
-        <li><a href="#comparison-tab">Comparison</a></li>
-        <li><a href="#trades-tab">Trades</a></li>
-      </ul>
-    </div>
-  </div>
+<ul>
+  <li><a class="dropdown-button" href="#" data-activates="inter-portfolio-nav"><i class="material-icons">menu</i></a></li>
+  <li><a href="#overview-tab">Overview</a></li>
+  <li><a href="#all-stocks-tab">All Stocks</a></li>
+  <li><a href="#analysis-tab">Analysis</a></li>
+  <li><a href="#comparison-tab">Comparison</a></li>
+  <li><a href="#trades-tab">Trades</a></li>
+</ul>
 </nav>
+<ul id="inter-portfolio-nav" class="dropdown-content">
+  <li><a href="#!">Portfolio 1</a></li>
+  <li><a href="#!">Portfolio 1</a></li>
+  <li><a href="#!">Portfolio 3</a></li>
+  <li><a href="#!">Portfolio 4</a></li>
+  <li><a href="#!">Portfolio 5</a></li>
+</ul>
