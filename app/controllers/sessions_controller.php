1528f9080fad176696778a18c84db0afb658ee42
Matt Breden <matt_bred@hotmail.com>
2016-07-14 14:38:27 -0700

    Redirect upon login

--- app/controllers/sessions_controller.php
+++ app/controllers/sessions_controller.php
@@ -2,26 +2,28 @@
 class SessionsController extends Spark\BaseController {
   public function create() {
     $params = $this->params;
-
     $user = User::find_by_email($params['user_email']);
  
     if ($user && $user->authenticate($params['user_password'])) {
-
+      // Logged in.
       $_SESSION['user_id'] = $user->id;
       $this->locals = array('message' => $_SESSION['user_id']);
-      $this->render('users/index.php');
 
+      if (count(current_user()->portfolios) == 0) {
+        redirect_to('/portfolios/new');
+      } else {
+        redirect_to('/portfolios/'.current_user()->portfolios[0]->id);
+      }
     } else {
-
-      $this->locals = array('message' => "all bad");
-      $this->render('users/index.php');
-
+      // Login doesn't match.
+      $this->locals = array('message' => "Login Failed");
+      $this->render('index.php');
     }
   }
 
   public function destroy() {
+    // Logged out.
     $_SESSION['user_id'] = null;
-
     $this->render('index.php');
   }
 }
