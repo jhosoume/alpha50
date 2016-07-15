fd4da35d022ac4d0a1c0a34182835f7b55f5f55e
triple-eh <aleckaleksandrov@gmail.com>
2016-07-14 17:25:56 -0700

    New portfolio table works

--- public/js/new_portfolio.js
+++ public/js/new_portfolio.js
@@ -1,5 +1,33 @@
 $(function() {
 
+  var totalPortfolioValue = 0;
+  var startingCapital = 1000000;
+
+  var portfolioTableData = $('.new-portfolio-data tbody');
+  portfolioTableData.children().each(function(idx,row) {
+    calculateTotalShareValue(row);
+  });
+
+  calculatePctOfTotal();
+
+  $('span.equity-holdings').text("$"+ Math.floor(totalPortfolioValue).toLocaleString());
+  $('span.cash-holdings').text("$" + Math.floor(startingCapital - totalPortfolioValue).toLocaleString());
+
+
+  var previousValue;
+  $('.new-portfolio-data tbody')
+    .on('change','.number-of-shares > input', function() {
+      var stockRow = $(this).parent().parent();
+      calculateTotalShareValue(stockRow);
+      calculatePctOfTotal();
+      $('span.equity-holdings').text("$"+ Math.floor(totalPortfolioValue).toLocaleString());
+      $('span.cash-holdings').text("$" + Math.floor(startingCapital - totalPortfolioValue).toLocaleString());
+    })
+
+
+
+
+  // prevents scrolling parent div when scrolled at the end of .new-portfolio-data
   var newPortfolio = $('.new-portfolio-data'),
       height = newPortfolio.height(),
       scrollHeight = newPortfolio.get(0).scrollHeight;
@@ -9,5 +37,25 @@ $(function() {
       e.preventDefault();
     }
   });
+
+
+  function calculateTotalShareValue(stockRow) {
+    var numberOfShares = $(stockRow).children('td.number-of-shares').children('input').val();
+    var sharePrice = $(stockRow).children('td.stock-price').text();
+    var totalValue = numberOfShares * sharePrice;
+    $(stockRow).children('td.total-value').text(Math.round(totalValue, 2));
+    totalPortfolioValue += totalValue;
+  }
+
+  function calculatePctOfTotal() {
+    portfolioTableData.children().each(function(idx,row) {
+      var numberOfShares = $(row).children('td.number-of-shares').children('input').val();
+      var sharePrice = $(row).children('td.stock-price').text();
+      var totalValue = numberOfShares * sharePrice;
+      var pctOfTotal = totalValue / totalPortfolioValue;
+      $(row).children('td.pct-of-total').text(Math.round(pctOfTotal * 100, 0) + "%");
+    });
+  }
+
 
 });
