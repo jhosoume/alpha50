df9a654b18b815fb59be5554c0087075fae56f5c
Matt Breden <matt_bred@hotmail.com>
2016-07-14 17:46:58 -0700

    Add basic 'all stocks' tab layout
    modify controller to set local variables
    modify the all_stocks view to include all the stocks details
    modify the new-portfolio.css to remove overflow y scrollbar
    add all_stocks.js to enable the materialize drop down select

--- app/controllers/portfolios_controller.php
+++ app/controllers/portfolios_controller.php
@@ -14,19 +14,26 @@ class PortfoliosController extends Spark\BaseController {
     }
   }
 
-public function show() {
+  public function show() {
     $params = $this->params;
-    $portfolio_id = $params['id'];
-    $this->locals = ['portfolio_id' => $portfolio_id];
+    $portfolio = Portfolio::find($params['id']);
+
+    $locals = [
+      'portfolio_id'=>$portfolio->id,
+      'portfolio'=> $portfolio,
+      'stocks_portfolios'=>$portfolio->stocks_portfolios,
+    ];
+
+    $this->locals = $locals;
     $this->render('portfolios/index.php');
   }
 
-public function new() {
-  $portfolio = new Portfolio();
-  $sql_recent_half_hourly_quotes = sql_recent_half_hourly_quotes();
-  $half_hourly_quotes = \HalfHourlyQuote::find_by_sql($sql_recent_half_hourly_quotes);
-  $this->locals = ['quotes' => $half_hourly_quotes];
-  $this->render('portfolios/new.php');
+  public function new() {
+    $portfolio = new Portfolio();
+    $sql_recent_half_hourly_quotes = sql_recent_half_hourly_quotes();
+    $half_hourly_quotes = \HalfHourlyQuote::find_by_sql($sql_recent_half_hourly_quotes);
+    $this->locals = ['quotes' => $half_hourly_quotes];
+    $this->render('portfolios/new.php');
   }
 }
 
