cee26c2a2bcd8ece66ec9dd9461c0ec317177672
Juliana Hosoume <ju.hosoume@gmail.com>
2016-07-15 12:10:50 -0700

    Add index to database

--- python/db/seeds/index_portfolio_seeder.py
+++ python/db/seeds/index_portfolio_seeder.py
@@ -5,9 +5,14 @@ sys.path.insert(0,parentdir)
 
 from orator.seeds import Seeder
 from helpers.get_portfolio_from_csv import get_portfolio_csv
+import models.stock 
+import models.user 
+import models.stocks_portfolio
+import models.portfolio 
+import models.trade 
 
 
-INDEX_DEFINITION = '../../csvs/alpha_50.csv'
+INDEX_DEFINITION = '../csvs/alpha_50.csv'
 
 class IndexPortfolioSeeder(Seeder):
 
@@ -15,12 +20,17 @@ class IndexPortfolioSeeder(Seeder):
         """
         Run the database seeds.
         """
-        self.db.table('portfolios').insert({
-            'name': 'Alpha50',
-            'cash': 0.0})
+        self.db.table('users').insert({
+            'email': 'admin@alpha50',
+            'password_hash': '1234'})
+        portfolio = models.user.User.where('email', 'admin@alpha50').first().portfolios().save(models.portfolio.Portfolio({'name': 'Alpha50', 'cash': 0.0}))
+        stocks = get_portfolio_csv(INDEX_DEFINITION)
+        
+        for stock in stocks:
+            stock_id = models.stock.Stock.where('ticker', stock['ticker']).first()
+            models.stocks_portfolio.StocksPortfolio({'stock_id': stock_id.id, 'portfolio_id': portfolio.id, 'quantity_held': stock['quantity']}).save()
 
 
-        stocks = get_portfolio_csv()
 
 
         
