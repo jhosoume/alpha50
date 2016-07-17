be602c93a7d06f45198518e2e5dfd0ecaa4ccfc8
Juliana Hosoume <ju.hosoume@gmail.com>
2016-07-16 19:26:40 -0700

    Add index valuations

--- python/db/seeds/index_valuations_seeder.py
+++ python/db/seeds/index_valuations_seeder.py
@@ -6,14 +6,12 @@ sys.path.insert(0,parentdir)
 from orator.seeds import Seeder
 import arrow
 from helpers.get_index_valuations import get_index_value_history 
-import models.stock 
 import models.user 
-import models.stocks_portfolio
 import models.portfolio 
-import models.trade 
+import models.portfolio_valuation
 
 
-INDEX_DEFINITION = '../csvs/alpha_50.csv'
+INDEX_VALUATIONS = '../csvs/index_valuations.csv'
 
 class IndexValuationsSeeder(Seeder):
 
@@ -21,23 +19,13 @@ class IndexValuationsSeeder(Seeder):
         """
         Run the database seeds.
         """
-        self.db.table('users').insert({
-            'email': 'admin@alpha50',
-            'password_hash': '1234'})
         user = models.user.User.where('email', 'admin@alpha50').first()
-        models.user.User.where('email', 'admin@alpha50').first().portfolios().save(models.portfolio.Portfolio({'name': 'Alpha50', 'total_cash': 0.0}))
         portfolio = models.portfolio.Portfolio.where('name', 'Alpha50').where('user_id', user.id).first()
-        stocks = get_portfolio_csv(INDEX_DEFINITION)
-        date_created = arrow.get('2009-01-01T01:00:00-07:00')
+        valuations = get_index_value_history(INDEX_VALUATIONS)
         
-        for stock in stocks:
-            stock_owner = models.stock.Stock.where('ticker', stock['ticker']).first()
-            stocks_portfolio = models.stocks_portfolio.StocksPortfolio({'stock_id': stock_owner.id, 'portfolio_id': portfolio.id, 'quantity_held': stock['quantity']})
-            stocks_portfolio.save()
-            stocks_portfolio = models.stocks_portfolio.StocksPortfolio.where('stock_id', stock_owner.id).where('portfolio_id', portfolio.id).first()
-            trade = models.trade.Trade({'stocks_portfolio_id': stocks_portfolio.id, 'quantity': stock['quantity'], 'price': stock['price']})
-            trade.created_at = date_created
-            trade.save()
+        for valuation in valuations:
+            valuation['portfolio_id'] = portfolio.id
+            models.portfolio_valuation.PortfolioValuation.create(valuation)
 
 
 
--- python/helpers/get_index_valuations.py
+++ python/helpers/get_index_valuations.py
@@ -7,12 +7,12 @@ sys.path.insert(0,parentdir)
 
 import arrow
 import csv
-from decimal import Decimal
 from models.stock import Stock
+from models.user import User
 from models.daily_quote import DailyQuote
 from models.portfolio import Portfolio
+from models.portfolio_valuation import PortfolioValuation
 
-CSV_FILE = '../csvs/index_valuations.csv'
 SECTORS = ['information_technology', 'energy', 'consumer_discretionary', 'health_care', 'industrials', 'telecommunications_services', 'financials', 'consumer_staples'] 
 
 
@@ -31,10 +31,25 @@ def get_index_value_history(csv_name):
         reader = csv.reader(fd)
         for nrow, row in enumerate(reader):
             date_prices.append({'created_at': datetime_from_string(row[0]),
-                           'portfolio_value': row[1]})
+                           'portfolio_value': float(row[1])})
             for indx, sector in enumerate(SECTORS):
-                date_prices[nrow][sector] = row[2 + indx]
+                date_prices[nrow][sector] = float(row[2 + indx])
     return date_prices
 
+if __name__ == '__main__':
+
+        INDEX_VALUATIONS = '../csvs/index_valuations.csv'
+
+        user = User.where('email', 'admin@alpha50').first()
+        portfolio = Portfolio.where('name', 'Alpha50').where('user_id', user.id).first()
+        valuations = get_index_value_history(INDEX_VALUATIONS)
+        for valuation in valuations:
+            import pdb; pdb.set_trace()
+            valuation['portfolio_id'] = portfolio.id
+            PortfolioValuation.create(valuation)
+            #pv = PortfolioValuation(valuation)
+            #pv.created_at = valuation['created_at'] 
+            #pv.save()
+            
 
 
