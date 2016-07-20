3c642948ffd6e9109e61072075920fdc799c045e
triple-eh <aleckaleksandrov@gmail.com>
2016-07-19 17:53:24 -0700

    Removed trading from overview tab

--- app/views/portfolios/portfolio-tab/analysis.php
+++ app/views/portfolios/portfolio-tab/analysis.php
@@ -20,23 +20,20 @@
   <div id="sector-overview-chart"></div>
   <div class="row">
     <div class="col m12">
-      <form method="POST" action="/trades" id="sectorStocksTradeForm">
-        <input type="hidden" name="portfolioId" value="<?=$portfolio->id?>">
-      </form>
       <table class='bordered highlight' id='sector-trades-table'>
         <thead class='teal-text darken-4'>
           <tr>
-            <th >Ticker</th>
+            <th>Ticker</th>
             <th>Company Name</th>
             <th>Last<br>Price</th>
             <th>Shares<br>Owned</th>
-            <th>Trade<br>Type</th>
-            <th>Trade<br>Quantity</th>
-            <th>Sub<br>Total</th>
+            <th>Value</th>
           </tr>
+        </thead>
           <tbody>
             <?php foreach($stocks_portfolios as $s_p): ?>
               <?php $ticker = $s_p->stock->ticker?>
+              <?php $value = number_format(floor($s_p->quantity_held * $s_p->stock->latest_price)) ?>
               <tr>
                 <td class="stock-ticker">
                   <input name="ticker<?=$ticker?>" type="text" form="sector
@@ -45,44 +42,12 @@
                 <td class="stock-name" data-sector='<?= $s_p->stock->sector ?>'><?= $s_p->stock->name ?></td>
                 <td class="stock-price"><?=$s_p->stock->latest_price?></td>
                 <td class="shares-number"><?= $s_p->quantity_held ?></td>
-                <td class="trade-type">
-                  <select name="<?=$ticker?>TradeType" form="sectorStocksTradeForm">
-                    <option value="buy">BUY</option>
-                    <option value="sell">SELL</option>
-                  </select>
-                </td>
-                <td class="trade-quantity"><input name="<?=$ticker?>TradeQuantity" type='number' value="0" min="0" form="sectorStocksTradeForm"></td>
-                <td class="sub-total">$0</td>
+                <td clss='stock-value'><?= $value ?></td>
               </tr>
             <?php endforeach ; ?>   
           </tbody>
-        </thead>
       </table>
     </div>
   </div>
-  <div class="all-stocks-checkout">
-    <div>
-      <p><strong>Buying</strong></p>
-      <p><strong>Selling</strong></p>
-      <p><strong>Net Cash</strong></p>
-    </div>
 
-    <div>
-      <p class="buyingMoney">$0</p>
-      <p class="sellingMoney">$0</p>
-      <p class="netMoney">$0</p>
-    </div>
-    <hr>
-    <div>
-      <p><strong>Adj. Cash</strong></p>
-      <p><strong>Adj. Equity</strong></p>
-    </div>
-
-    <div>
-      <p class="adjustedCash">$0</p>
-      <p class="adjustedEquity">$0</p>
-    </div>
-    <br>
-    <button class="btn" form="sectorStocksTradeForm">Checkout</button>
-  </div>
 </div>
